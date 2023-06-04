cd62c5185a45894bca8d8e652850566be260e3af
jxfzzzt <1160490625@qq.com>
2023-06-04 21:50:07 +0800

    finish 00_angr

---
+++ 00_angr_find/answer.py
@@ -0,0 +1,13 @@
+import angr
+
+print('hello')
+
+proj = angr.Project('./00_angr_find')
+init_state = proj.factory.entry_state()
+sm = proj.factory.simulation_manager(init_state)
+
+sm.explore(find=0x08048678) # 该地址通过ida工具得到
+
+found_state = sm.found[0] # 找到一个满足条件的输入
+
+print(found_state.posix.dumps(0)) # 输出JXWVXRKX
