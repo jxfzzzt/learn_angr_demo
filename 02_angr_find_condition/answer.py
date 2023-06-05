4f1d33d809325239a043e2617c53c1b9f21ffda5
jxfzzzt <1160490625@qq.com>
2023-06-05 23:40:58 +0800

    finish 03

--- 02_angr_find_condition/answer.py
+++ 02_angr_find_condition/answer.py
@@ -1,4 +1,24 @@
 import angr
 
-proj = angr.Project('./02_angr_find_condition')
 
+def main():
+    proj = angr.Project('./02_angr_find_condition')
+    init_state = proj.factory.entry_state()
+    sm = proj.factory.simulation_manager(init_state)
+
+    def is_good(state):
+        return b'Good Job' in state.posix.dumps(1)
+
+    def is_bad(state):
+        return b'Try again' in state.posix.dumps(1)
+
+    sm.explore(find=is_good, avoid=is_bad)
+
+    if sm.found:
+        found_state = sm.found[0]
+        print('Solution is {}'.format(found_state.posix.dumps(0)))
+    else:
+        raise Exception("Solution not found")
+
+if __name__ == '__main__':
+    main()
---
+++ 03_angr_symbolic_registers/answer.py
@@ -0,0 +1,36 @@
+import angr
+
+def main():
+    proj = angr.Project('03_angr_symbolic_registers')
+    start_addr = 0x08048980
+    init_state = proj.factory.blank_state(addr=start_addr)
+
+    pass1 = init_state.solver.BVS('pass1', 32)
+    pass2 = init_state.solver.BVS('pass2', 32)
+    pass3 = init_state.solver.BVS('pass3', 32)
+
+    init_state.regs.eax = pass1
+    init_state.regs.ebx = pass2
+    init_state.regs.edx = pass3
+
+    sm = proj.factory.simulation_manager(init_state)
+
+    def is_good(state):
+        return b'Good Job' in state.posix.dumps(1)
+
+    def is_bad(state):
+        return b'Try again' in state.posix.dumps(1)
+
+    sm.explore(find=is_good, avoid=is_bad)
+
+    if sm.found:
+        found_state = sm.found[0]
+        password1 = found_state.solver.eval(pass1)
+        password2 = found_state.solver.eval(pass2)
+        password3 = found_state.solver.eval(pass3)
+        print('Solution: {:x} {:x} {:x}'.format(password1, password2, password3))
+    else:
+        raise Exception("Solution not found")
+
+if __name__ == '__main__':
+    main()
