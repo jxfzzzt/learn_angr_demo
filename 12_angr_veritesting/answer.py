707244baaec2246c10ea264f5430bf6cc270c746
jxfzzzt <1160490625@qq.com>
2023-06-08 09:18:35 +0800

    finish 12 and 13

--- 12_angr_veritesting/answer.py
+++ 12_angr_veritesting/answer.py
@@ -3,6 +3,22 @@ import angr
 def main():
     proj = angr.Project('12_angr_veritesting')
 
+    init_state = proj.factory.entry_state()
+    sm = proj.factory.simulation_manager(init_state, veritesting=False)
+
+    def is_good(state):
+        return b'Good Job' in state.posix.dumps(1)
+    def is_bad(state):
+        return b'Try again' in state.posix.dumps(1)
+
+    sm.explore(find=is_good, avoid=is_bad)
+
+    if sm.found:
+        found_state = sm.found[0]
+        password = found_state.posix.dumps(0).decode('utf-8')
+        print('Solution: {}'.format(password))  # OQSUWYACEGIKMOQSUWYACEGIKMOQSUWY
+    else:
+        raise Exception("Solution not found")
 
 
 if __name__ == '__main__':
--- 13_angr_static_binary/answer.py
+++ 13_angr_static_binary/answer.py
@@ -7,9 +7,14 @@ def main():
     strcmp_addr = 0x08048280
     puts_addr = 0x0804F350
     scanf_addr = 0x0804ED80
+    libc_start_main_addr = 0x08048D10
 
     init_state = proj.factory.entry_state()
-
+    proj.hook(printf_addr, angr.SIM_PROCEDURES['libc']['printf']())
+    proj.hook(strcmp_addr, angr.SIM_PROCEDURES['libc']['strcmp']())
+    proj.hook(puts_addr, angr.SIM_PROCEDURES['libc']['puts']())
+    proj.hook(scanf_addr, angr.SIM_PROCEDURES['libc']['scanf']())
+    proj.hook(libc_start_main_addr, angr.SIM_PROCEDURES['glibc']['__libc_start_main']())
 
     def is_good(state):
         return b'Good Job' in state.posix.dumps(1)
@@ -23,7 +28,7 @@ def main():
     if sm.found:
         found_state = sm.found[0]
         password = found_state.posix.dumps(0).decode('utf-8')
-        print('Solution: {}'.format(password))
+        print('Solution: {}'.format(password)) # PNMXNMUD
     else:
         raise Exception('Solution not found')
 
